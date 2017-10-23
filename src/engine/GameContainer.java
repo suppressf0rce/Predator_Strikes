ff919c6b7d2148b76b38de7713ce9b6c4a30c8a4
suppressf0rce <suppressforce@gmail.com>
2017-10-23 22:01:00 +0000

    Initial commit

---
+++ src/engine/GameContainer.java
@@ -0,0 +1,34 @@
+package engine;
+
+/**
+ * This class contains our game
+ * <p>
+ * The way this engine is going to be designed is separate from the game
+ * This engine can be used multiple times. Its going to be self independent
+ * and not meshed with the game
+ */
+public class GameContainer {
+
+    //===>>Constructor<<===//
+    public GameContainer() {
+
+    }
+
+
+    //===>Methods<<===//
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void run() {
+
+    }
+
+    private void dispose() {
+
+    }
+}
